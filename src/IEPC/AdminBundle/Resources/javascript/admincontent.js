d07bb10b2ec429d9973df8bd439e8209e0ea40d5
iZam b <izma@mabkil.com>
2016-04-19 09:39:55 -0500

    -- PC Menu

--- src/IEPC/AdminBundle/Resources/javascript/admincontent.js
+++ src/IEPC/AdminBundle/Resources/javascript/admincontent.js
@@ -14,7 +14,6 @@ function saveContent() {
     var $content = $(this).parents('form').first(),
         id       = $('#page_id').val(),
         value    = $content.find('textarea').val();
-
     $.post('/admin/content/edit/' + id , {content: value}, function(data){
         console.log(data);
     }, 'json');
