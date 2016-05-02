edb60ba937ee84b6f3e01e97e7b190065b1d9739
iZam b <izma@mabkil.com>
2016-05-02 13:04:10 -0500

    -- Updated versions -- Decouple content and webpage layouts

--- src/IEPC/ContentBundle/Controller/WebPageController.php
+++ src/IEPC/ContentBundle/Controller/WebPageController.php
@@ -1,6 +1,7 @@
 <?php namespace IEPC\ContentBundle\Controller;
 
 use IEPC\ContentBundle\Entity\WebPage;
+use IEPC\ContentBundle\Model\ContentInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
@@ -10,6 +11,10 @@ use Doctrine\ORM\NoResultException;
  */
 class WebPageController extends Controller
 {
+    /**
+     * @param string $path
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function renderPathAction($path = '/')
     {
         $em = $this->getDoctrine()->getManager();
@@ -25,13 +30,52 @@ class WebPageController extends Controller
         return $this->RenderWebPage($webPage);
     }
 
+    /**
+     * @param \IEPC\ContentBundle\Entity\WebPage $webPage
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function RenderWebPage(WebPage $webPage)
     {
         $layout = $webPage->getActiveLayout() ?: $this->getParameter('default_layout');
+        $bundle = $this->getBundleNameFromEntity(get_class($webPage->getContent()), $this->get('kernel')->getBundles());
 
-        return $this->render('IEPCContentBundle:WebPage:index.html.twig', [
-            'webpage' => $webPage,
-            'layout'  => $layout
+        $template = "{$bundle}:_webpage:{$layout}.html.twig";
+
+        return $this->render($template, [
+            'webpage' => $webPage
         ]);
+    }
+
+    /**
+     * @param \IEPC\ContentBundle\Model\ContentInterface $content
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @todo move to ContentController and merge with current implementation
+     */
+    public function RenderContentAction(ContentInterface $content)
+    {
+        $bundle = $this->getBundleNameFromEntity(get_class($content), $this->get('kernel')->getBundles());
+
+        $template = "{$bundle}:_content/page:default.html.twig";
+
+        return $this->render($template, [
+            'content' => $content
+        ]);
+    }
+
+
+    // @todo put this in a service
+    protected static function getBundleNameFromEntity($entityNamespace, $bundles)
+    {
+        $dataBaseNamespace = substr($entityNamespace, 0, strpos($entityNamespace, '\\Entity\\'));
+
+        foreach ($bundles as $type => $bundle) {
+            $bundleRefClass = new \ReflectionClass($bundle);
+            if ($bundleRefClass->getNamespaceName() === $dataBaseNamespace) {
+                return $type;
+            }
+        }
+
+        return null;
     }
 }
